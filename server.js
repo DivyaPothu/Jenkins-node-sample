c2621010856253ca93e58c15438c4d7800e34856
Divya Pothu <divya.pothu@tcs.com>
2017-11-13 18:46:26 +0530

    make it better

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res){
-    res.send("hello jenkins.....");
+	res.send("continuous deployment! Hello Heroku");
+	//res.send("hello heroku");
+    //res.send("hello jenkins.....");
   });
 
 app.listen(5000);
-console.log('hello');
+console.log('port is running at 5000');
