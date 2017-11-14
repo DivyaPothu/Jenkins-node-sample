626eaf1bb57dc57f840e61ccedb36920e4783a90
Divya Pothu <divya.pothu@tcs.com>
2017-11-14 16:34:37 +0530

    removed console

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ var app = express();
 var port = process.env.PORT || 4000;
 
 app.get('/',function(req,res) {
-  res.send("Continuous deployment!! hello jenkins");
+  res.send("Continuous deployment!! hello jenkinss");
 });
 
 app.listen(port);
-console.log('hello jenkins');
